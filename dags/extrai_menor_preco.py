266b773687e9a9a9ac6cb8428f31339f9b6ffe3f
Arii19 <arianer19@hotmail.com>
2025-06-18 15:02:02 -0300

    subindo dag corrigindo

--- dags/extrai_menor_preco.py
+++ dags/extrai_menor_preco.py
@@ -2,6 +2,13 @@ import pendulum
 import requests
 from bs4 import BeautifulSoup
 from airflow.decorators import dag, task
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.macros import ds_add
+from os.path import join
+import pandas as pd
+import os
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
@@ -11,6 +18,7 @@ urls = {
     "Amazon": "https://www.amazon.com.br/Apple-iPhone-16-Plus-128/dp/B0DGMPCP48/ref=sr_1_1_sspa?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2SI0HXNAIXBID&dib=eyJ2IjoiMSJ9.mKL0CDUL-izWEwZ_TrTpr6w8TMYo7WxevYEgLmU-FFa1Jc77GmzYKxvLfFP-MGP9Uq_eiByprCq8sL8E8RC_4CX2H-S6pNwnJfXMEviY3RWB5lt0auECPzLf4sezMCYIvrJyI5wKcLKFCfZbvxjGWoatJjLqp4UByhnwSA8J8MjtIr8UlRrclltnfYg8JFFvrKNr3tHFDKtj1x_Sh2uSohMQYabyS5H5YnbDT-LhQUKe0mPNhqUURh3-kLZNxKiK5HZcKTICLhyr55wafexlgzZn2Jkrx_zUVnGulzk7JiU.Hyf-Ast-xTjHAn9kodtcfdYBOVDdM55TFWRhbVMrXtY&dib_tag=se&keywords=iphone%2B16&qid=1750269162&sprefix=iphone16%2Caps%2C191&sr=8-1-spons&ufe=app_do%3Aamzn1.fos.9e6a115c-05b9-4b96-8e1c-b1f9ce2ac1a6&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1",
     "CasasBahia": "https://www.casasbahia.com.br/apple-iphone-16-128gb-branco/p/55067622?utm_source=Google&utm_medium=BuscaOrganica&utm_campaign=DescontoEspecial",
     "Magalu": "https://www.magazineluiza.com.br/iphone-16-128gb-5g-branco-tela-61-camera-48mp-apple/p/ddd5a7h697/te/teip/",
+    "Americanas": "https://www.americanas.com.br/produto/55062209"
 }
 
 def extrair_amazon(url):
@@ -52,3 +60,33 @@ def extrair_americanas(url):
 @dag(
     schedule='@daily',
     start_date=pendulum.date_
+)
+def main_dag():
+
+    @task()
+    def extrai_precos():
+        precos = {}
+        for loja, url in urls.items():
+            if loja == "Amazon":
+                precos["Amazon"] = extrair_amazon(url)
+            elif loja == "CasasBahia":
+                precos["CasasBahia"] = extrair_casas_bahia(url)
+            elif loja == "Magalu":
+                precos["Magalu"] = extrair_magalu(url)
+            elif loja == "Americanas":
+                precos["Americanas"] = extrair_americanas(url)
+        return precos
+
+    @task()
+    def salva_precos(precos):
+        import json
+        from datetime import datetime
+        data_atual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        precos["data_coleta"] = data_atual
+        with open("/path/to/seu_arquivo.json", "a") as f:
+            f.write(json.dumps(precos) + "\n")
+
+    precos = extrai_precos()
+    salva_precos(precos)
+
+main_dag()
